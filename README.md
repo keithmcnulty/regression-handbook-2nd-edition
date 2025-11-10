7b057c0f9c387eed31bea3962ec0316f22083180
Keith McNulty <keith.mcnulty@gmail.com>
2025-11-10 16:19:42 +0000

    Update

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ Handbook of Regression Modeling in People Analytics. 2nd edition
 This is the repository for the (in development) 2nd edition of the textbook [Handbook of Regression Modeling in People Analytics](http://peopleanalytics-regression-book.org/).  
 
 Use Issues to flag corrections or requests for additional content.
-
-## Solutions to exercises
-
-Solutions to exercises requested by readers are progressively being posted [here](https://solutions.peopleanalytics-regression-book.org).
---
+++ _common.R
@@ -0,0 +1,21 @@
+# library(reticulate)
+# library(JuliaCall)
+
+if (knitr::is_latex_output()) {
+  options(width = 100)
+}
+
+knitr::opts_chunk$set(out.width = "90%")
+
+# library(metathis)
+#
+# if (knitr::is_html_output()) {
+#   meta() |>
+#     meta_google_scholar(
+#       title = "Handbook of Regression Modeling in People Analytics: With Examples in R and Python (2nd Edition)",
+#       author = "Keith McNulty",
+#       publication_date = "2026"
+#       # isbn = "9781003194156"
+#     )
+# }
+
